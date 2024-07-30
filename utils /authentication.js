6c96bbe3bcaca5682713895ee24c65de4d064f54
Meadoughnut <127433011+Meadoughnut@users.noreply.github.com>
2024-07-30 19:54:21 -0400

    Controllers initiated, utils added for more authentication and a trial model

---
+++ utils /authentication.js
@@ -0,0 +1,12 @@
+// This utill is necessary for to redirect the user who intent to sign up again
+
+const withAuth = (req, res, next) => {
+    if (!req.session.user_id) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = withAuth;
+  
