dd4edab9e6bc821ea7a65b6a61b2aa842a4cd5ce
mameami26 <mameami26@gmail.com>
2024-08-01 19:49:26 -0400

    update routes path

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
-const { User } = require('../../models/User');
-const withAuth = require('../../utils/auth');
+const {User} = require('../../models');
 
-router.post('/register', async (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
     req.session.save(() => {
@@ -11,6 +10,7 @@ router.post('/register', async (req, res) => {
       res.status(200).json(userData);
     });
   } catch (err) {
+    console.log(err);
     res.status(400).json(err);
   }
 });
