c025ea0136a1fd467360e5b78a7862d848f543e5
mameami26 <mameami26@gmail.com>
2024-08-05 23:07:31 -0400

    checkpoint od1

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const sess = {
-  secret: process.env.SECRET,
+  secret: Super Secret,
   cookie: {},
   resave: false,
   saveUninitialized: true,
